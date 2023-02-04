68e80a070bda2e57c1973feac651c513c772c666
NagaChaitanya Vellanki <pnagato@protonmail.com>
2023-02-04 14:55:33 -0800

    nvim-cursorline

--- init.lua
+++ init.lua
@@ -223,6 +223,20 @@ local config = {
 
       -- You can also add new plugins here as well:
       -- Add plugins, the packer syntax without the "use"
+         { "yamatsum/nvim-cursorline",
+            require('nvim-cursorline').setup {
+            cursorline = {
+                enable = true,
+                timeout = 1000,
+                number = false,
+            },
+            cursorword = {
+                enable = true,
+                min_length = 3,
+                hl = { underline = true },
+            },
+            },
+         },
          { "sindrets/diffview.nvim",
            requires = "nvim-lua/plenary.nvim",
          },
